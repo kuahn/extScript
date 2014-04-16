3e1d542d1d7478fdee71a84f922d1d4248a0f901
KUAHN <z@kuahn.com>
2014-04-16 17:23:37 +0900

    Update addUi.js

--- addUi.js
+++ addUi.js
@@ -1,42 +1,46 @@
-(function($) {
-	$( document ).ready(function() {
-		$('.leftbrand').hover(
-			function() {
-				var title = $(this).attr('title')
-				,	hoverTitle;
-
-				switch (title) {
-					case "PRINCE TOP" :
-						hoverTitle = "남자 상의"
-						break;			
-					case "PRINCE BOTTOM" :
-						hoverTitle = "남자 하의"
-						break;			
-					case "PRINCESS TOP" :
-						hoverTitle = "여자 상의"
-						break;			
-					case "PRINCESS BOTTOM" :
-						hoverTitle = "여자 하의"
-						break;			
-					case "PRINCESS OPS" :
-						hoverTitle = "여자 원피스"
-						break;			
-					case "SET" :
-						hoverTitle = "상하의"
-						break;			
-					case "CAP" :
-						hoverTitle = "모자"
-						break;			
-					case "SHOES" :
-						hoverTitle = "신발"
-						break;
-				}
-
-				$(this).text(hoverTitle);
-			}, 
-			function() {
-				$(this).text($(this).attr('title'));
-			}
-		);		
-	});	
-}(jQuery));
+        (function($) {
+        	$( document ).ready(function() {
+            	$('.leftbrand').hover(
+            		function() {
+            			var title = $(this).parent('a').attr('title')
+            			,	hoverTitle;
+        
+                        console.log('title' + title);
+        
+            			switch (title) {
+            				case "PRINCE TOP" :
+            					hoverTitle = "남자 상의";
+            					break;
+            				case "PRINCE BOTTOM" :
+            					hoverTitle = "남자 하의";
+            					break;
+            				case "PRINCESS TOP" :
+            					hoverTitle = "여자 상의";
+            					break;
+            				case "PRINCESS BOTTOM" :
+            					hoverTitle = "여자 하의";
+            					break;
+            				case "PRINCESS OPS" :
+            					hoverTitle = "여자 원피스";
+            					break;
+            				case "SET" :
+            					hoverTitle = "상하의";
+            					break;
+            				case "CAP" :
+            					hoverTitle = "모자";
+            					break;
+            				case "SHOES" :
+            					hoverTitle = "신발";
+            					break;
+            			}
+        
+                        console.log('hoverTitle' + hoverTitle);
+        
+            			$(this).text(hoverTitle);
+            		},
+            		function() {
+            			$(this).text($(this).parent('a').attr('title'));
+            		}
+            	);
+        	});	
+        }(jQuery));
